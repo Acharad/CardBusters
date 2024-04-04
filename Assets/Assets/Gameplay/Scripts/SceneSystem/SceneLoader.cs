2736681a92744e8510fbb819d096cd08fc75d364
Acharad <iahmetkavras@gmail.com>
2024-04-04 13:17:07 +0300

    spalsh scene and scene loader added

---
+++ Assets/Assets/Gameplay/Scripts/SceneSystem/SceneLoader.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Sirenix.OdinInspector;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using AsyncOperation = UnityEngine.AsyncOperation;
+
+namespace Assets.Gameplay.Scripts.SceneSystem
+{
+    public class SceneLoader
+    {
+        public event Action<SceneLoadData> OnSceneLoadStart;
+        public event Action<SceneLoadData> OnSceneLoadFinished;
+        private string _oldScene;
+        private string _currentScene;
+        private readonly TimeSpan _timeout;
+
+        public SceneLoader(TimeSpan timeout)
+        {
+            _timeout = timeout;
+        }
+        
+        public string OldScene => _oldScene;
+        public string CurrentScene
+        {
+            get => _currentScene = string.IsNullOrEmpty(_currentScene)
+                ? SceneManager.GetActiveScene().name
+                : _currentScene;
+            private set
+            {
+                _oldScene = _currentScene;
+                _currentScene = value;
+            }
+        }
+        public string SceneToLoad { get; private set; }
+        
+        private Task _runningTask;
+        // private readonly TaskSequence _sequenceBeforeLoad;
+        // private readonly TaskSequence _sequenceAfterLoad;
+        
+        [Button]
+        public void LoadScene(string sceneToLoad)
+        {
+            SceneToLoad = sceneToLoad;
+            LoadSceneAsync();
+        }
+
+
+        private Task LoadSceneAsync(string sceneToLoad)
+        {
+            if (_runningTask != null)
+                return _runningTask;
+            
+
+            SceneToLoad = sceneToLoad;
+            _runningTask = LoadSceneAsync();
+            return _runningTask;
+        }
+
+        private async Task LoadSceneAsync()
+        {
+            using var cts = new CancellationTokenSource(_timeout);
+            var asyncOperation = SceneManager.LoadSceneAsync(SceneToLoad);
+
+            asyncOperation.allowSceneActivation = false;
+            OnSceneLoadStart?.Invoke(new SceneLoadData(CurrentScene, SceneToLoad));
+
+            await LoadSceneInternal(asyncOperation);
+            
+            OnSceneLoadFinished?.Invoke(new SceneLoadData(OldScene, CurrentScene));
+
+
+            _runningTask = null;
+        }
+        
+        private async Task LoadSceneInternal(AsyncOperation asyncOp)
+        {
+            while (!asyncOp.isDone)
+            {
+                if (asyncOp.progress >= 1f)
+                    asyncOp.allowSceneActivation = true;
+                await Task.Delay(15);
+            }
+
+            CurrentScene = SceneToLoad;
+            SceneToLoad = null;
+            Debug.Log($"Scene Loaded {CurrentScene}, time: {Time.time}");
+        }
+
+        public class SceneLoadData
+        {
+            public string OldScene;
+            public string NewScene;
+
+            public SceneLoadData(string oldScene, string newScene)
+            {
+                OldScene = oldScene;
+                NewScene = newScene;
+            }
+        }
+    }
+    
+}
